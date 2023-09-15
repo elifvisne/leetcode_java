bba1f8aaa9f13e45751f908cef12f6208e19ac0b
Elif Vişne <93011568+elifvisne@users.noreply.github.com>
2023-09-15 16:46:06 +0300

    Time: 0 ms (100.00%), Space: 40.8 MB (19.34%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle == null || haystack == null) {
+            return -1;
+        }
+        
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+        
+        if (needleLength == 0) {
+            return 0;
+        }
+        
+        if (haystackLength < needleLength) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            int j;
+            for (j = 0; j < needleLength; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            
+            if (j == needleLength) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
